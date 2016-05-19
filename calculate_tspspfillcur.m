d08538687fdaed1e550d3195c29cb70a441b5446
ismail akbudak <akbudak.ismail@yandex.com>
2016-05-19 23:51:21 +0300

    calculate vrpsweep ad tspfillcur

---
+++ calculate_tspspfillcur.m
@@ -0,0 +1,46 @@
+function [result] = calculate_tspspfillcur(form)
+% Example:
+% load 'imbros' % Loads XY, Name
+% calculate_vrpsavings(imbros)
+close
+result = {};
+XY = form.XY;
+C = dists(XY,XY,'km');
+tic;
+loc = tspspfillcur(XY);
+TD = locTC(loc,C)
+result.Time = toc;
+
+makemap(XY)
+h = pplot(XY,'r.');
+pplot(XY,form.Name(1:size(XY,1)));
+pplot({loc},XY,'g')
+
+fprintf('%f\n',TD);
+names = {};
+for j = 1:length(loc) - 1
+    names{j} = form.Name(loc(j));
+end
+result.names = names;
+
+% Check times
+number = 15;
+t = zeros(1,number);
+for n = 1:number
+    tic;
+	loc = tspspfillcur(XY);
+	TD = locTC(loc,C)
+    t(n) = toc;
+end
+
+result.loc = loc;
+result.TD = TD;
+result.times = t;
+result.mean_times = mean(result.times);
+
+figure(2)
+plot(t)
+grid on
+str = sprintf('TSP Spacefilling curve: Mean Calculation Times = %f and %d number of execution', result.mean_times,length(t));
+fprintf('%s\n\n',str)
+title(str)
