b09e56344c13ed0cd5f044c06e6fecedb4009034
ismail akbudak <akbudak.ismail@yandex.com>
2016-05-21 20:29:27 +0300

    change title for vrpinsert

--- calculate_vrpinsert.m
+++ calculate_vrpinsert.m
@@ -11,13 +11,14 @@ h = pplot(XY,'r.');
 pplot(XY,form.Name(1:size(XY,1)));
 tic;
 [loc,TC] = vrpinsert(C,[],[],[],[],h);
-result.Time = toc;
-fprintf('%f\n',TC);
+result.Time = toc; 
 names = {};
 for j = 1:length(loc{1}) - 1
     names{j} = form.Name(loc{1}(j));
 end
 result.names = names;
+result.loc = loc;
+result.TC = TC;
 
 % Check times
 number = 15;
@@ -27,9 +28,7 @@ for n = 1:number
     [loc,TC] = vrpinsert(C,[],[],[],[],h);
     t(n) = toc;
 end
-
-result.loc = loc;
-result.TC = TC;
+title(sprintf('VRP Insert: Final TC = %f and %d Loc Seqs', sum(TC),length(TC)))
 result.times = t;
 result.mean_times = mean(result.times);
 
@@ -37,5 +36,4 @@ figure(2)
 plot(t)
 grid on
 str = sprintf('VRP Insert: Mean Calculation Times = %f and %d number of execution', result.mean_times,length(t));
-fprintf('%s\n\n',str)
 title(str)
